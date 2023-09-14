df4efc6950c8e5b0e87321336f232f06cb593f5f
patamarcelo <patamarcelo@gmail.com>
2023-09-13 21:09:33 -0300

    added env file

--- screens/Map.js
+++ screens/Map.js
@@ -1,16 +1,43 @@
 import MapView, { Marker } from "react-native-maps";
 import { StyleSheet, View } from "react-native";
+import { useState } from "react";
 
 const Map = () => {
+	const [selectedLocation, setSelectedLocation] = useState();
 	const region = {
 		latitude: 37.78,
 		longitude: -122.43,
 		latitudeDelta: 0.0922,
 		longitudeDelta: 0.0421
 	};
+
+	const selectLocationHandler = (event) => {
+		// console.log(event);
+		const lat = event.nativeEvent.coordinate.latitude;
+		const lng = event.nativeEvent.coordinate.longitude;
+		console.log(lat, lng);
+		setSelectedLocation({
+			lat: lat,
+			lng: lng
+		});
+	};
 	return (
 		<View style={styles.container}>
-			<MapView style={styles.map} initialRegion={region}></MapView>
+			<MapView
+				style={styles.map}
+				initialRegion={region}
+				onPress={selectLocationHandler}
+			>
+				{selectedLocation && (
+					<Marker
+						title="Picked Location"
+						coordinate={{
+							latitude: selectedLocation.lat,
+							longitude: selectedLocation.lng
+						}}
+					/>
+				)}
+			</MapView>
 		</View>
 	);
 };
--- utils/Location.js
+++ utils/Location.js
@@ -1,4 +1,4 @@
-const GOOGLE_API = "AIzaSyC8YC113eFgg-3ImeTxLHO1iJ2hnDf8d0E";
+import { GOOGLE_API } from "@env";
 
 export const getMapPreview = (lat, long) => {
 	return (imgPreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${long}&zoom=14&size=400x200&maptype=roadmap&markers=color:blue%7Clabel:S%7C${lat},${long}&key=${GOOGLE_API}`);
